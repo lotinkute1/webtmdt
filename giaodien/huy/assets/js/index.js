f63113f62f6f1dad89633b8e7883b55f556892f1
Huy X <lotinkute1@gmail.com>
2021-12-10 00:17:09 +0700

    lam giao dien chi tiet san pham

--- giaodien/huy/assets/js/index.js
+++ giaodien/huy/assets/js/index.js
@@ -1,8 +1,21 @@
+// =================== hide scorll top button
 const scrollTop = document.querySelector(".scroll-top");
+//==================== fixed nav
+const header= document.querySelector("#header");
 
-function hideScrollBtn(){
+function hideElement(){
     if(window.pageYOffset <= 0) scrollTop.classList.add("hidden");
     else scrollTop.classList.remove("hidden")
+
+    if(window.pageYOffset >= 175) header.classList.add("hidden-header");
+    else header.classList.remove("hidden-header")
+
+    console.log(window.pageYOffset );
+    
 };
-hideScrollBtn();
-window.addEventListener("scroll",hideScrollBtn)
+hideElement();
+window.addEventListener("scroll",hideElement)
+
+
+
+
