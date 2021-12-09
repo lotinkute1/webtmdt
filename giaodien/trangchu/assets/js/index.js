7572154203ad5171d52d70fa2c820cd62e78d6e9
Huy X <62562870+lotinkute1@users.noreply.github.com>
2021-12-09 21:56:08 +0700

    xoa folder trang chu

--- giaodien/trangchu/assets/js/index.js
+++
@@ -1,8 +0,0 @@
-const scrollTop = document.querySelector(".scroll-top");
-
-function hideScrollBtn(){
-    if(window.pageYOffset <= 0) scrollTop.classList.add("hidden");
-    else scrollTop.classList.remove("hidden")
-};
-hideScrollBtn();
-window.addEventListener("scroll",hideScrollBtn)
