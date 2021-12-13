b650960412fefe0c8a45e9af456756ef617a11a2
Huy X <lotinkute1@gmail.com>
2021-12-13 16:58:54 +0700

    them product info va comments

--- giaodien/huy/assets/js/index.js
+++ giaodien/huy/assets/js/index.js
@@ -16,6 +16,55 @@ function hideElement(){
 hideElement();
 window.addEventListener("scroll",hideElement)
 
+// increment and decrement quantily option
+const increQuantilyBtnOp= document.querySelector(".quantily-wrapper .btn-option:last-child");
+const decreQuantilyBtnOp= document.querySelector(".quantily-wrapper .btn-option:first-child");
+const quantilyInputOp=document.querySelector(".quantily-options__number")
 
 
+increQuantilyBtnOp.onclick = () =>{
+    let numberOption=Number( quantilyInputOp.value)
+    if(numberOption < 999){
+        
+        numberOption+=1;
+        quantilyInputOp.value=numberOption;
+    }
+}
+decreQuantilyBtnOp.onclick = () =>{
+    let numberOption=Number( quantilyInputOp.value)
+    if(numberOption>0&&numberOption<=999){
+        numberOption-=1;
+        quantilyInputOp.value=numberOption;
 
+    }
+}
+quantilyInputOp.oninput = (e) =>{
+    
+    if(Number.isInteger(Number(e.target.value)))
+    {
+        console.log(Number(e.target.value));
+        if(Number(e.target.value) < 999&&Number(e.target.value) >=0)
+        {
+            quantilyInputOp.value=Number(e.target.value);
+        }else quantilyInputOp.value=999;
+            
+    }
+    else quantilyInputOp.value=0;
+    // if(e.target.value>=999 || e.target.value<0){
+    //     quantilyInputOp.value=0;
+    // }else quantilyInputOp.value=e.target.value;
+}
+
+
+// change like color
+const likeBtns = document.querySelectorAll(".btn-like");
+const likeCount= document.querySelectorAll(".comment__actions__like-count")
+console.log(likeCount);
+likeBtns.forEach((likeBtn,index)=>{
+    likeBtn.onclick=(e)=>{
+        e.target.closest(".btn-like").classList.toggle("active");
+        if(e.target.closest(".btn-like").classList.includes("active"))
+            likeCount[index].value-=1;
+        else likeCount[index].value+=1;
+    }
+})
